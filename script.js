cdc4b0883388913eea2cbd94a00a51ab9ac66814
Traveller Coder <guillermobukowski@gmail.com>
2024-09-16 13:16:48 -0300

    agregado de scrollreveal

--- script.js
+++ script.js
@@ -5,3 +5,16 @@ menu.onclick = () => {
     menu.classList.toggle('bx-x');
     navList.classList.toggle('open');
 }
+
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '80px',
+    duration: 2000,
+    delay: 450,
+    reset: true
+});
+
+sr.reveal('.hero-text', {delay: 200, origin: 'bottom'});
+sr.reveal('.hero-img', {delay: 200, origin: 'right'});
+sr.reveal('.icons', {delay: 550, origin: 'left'});
+sr.reveal('header', {delay: 300, origin: 'top'});
